3e3d63ecefdf3f842206456a0fdd6830a8851226
floshen18 <88593707+floshen18@users.noreply.github.com>
2022-03-27 23:13:51 -0400

    Create run_lorra_classifier copy.sh

---
+++ run_lorra_classifier copy.sh
@@ -0,0 +1,6 @@
+export CUDA_VISIBLE_DEVICES=0
+export PYTHONPATH=.
+python mmf_cli/run.py dataset=textvqa \
+  model=m4c \
+  config=projects/m4c/configs/textvqa/lorra_classifier_defaults.yaml \
+  env.save_dir=./save/m4c
